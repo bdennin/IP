ced0e801288577fe58da288099be9282fd3304c2
bdennin <brandondenning25@gmail.com>
2017-03-16 21:35:18 -0600

    fix slow ass response time

---
+++ Macros/eqgame.bat
@@ -0,0 +1,8 @@
+start /d "C:\Users\Jimbob\Downloads\IP" macroquest2.exe
+start /d "C:\Users\Jimbob\Downloads\IP" eqbcserver.exe
+start /d "C:\everquest UF" eqgame.exe patchme /login:bdens01
+start /d "C:\everquest UF" eqgame.exe patchme /login:bdens02
+start /d "C:\everquest UF" eqgame.exe patchme /login:bdens03
+start /d "C:\everquest UF" eqgame.exe patchme /login:bdens04
+start /d "C:\everquest UF" eqgame.exe patchme /login:bdens05
+start /d "C:\everquest UF" eqgame.exe patchme /login:bdens06
