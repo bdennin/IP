25d34f8d1f79d2a77dc02ff0d1b9fb6a6b2b8c5f
bdennin <brandondenning25@gmail.com>
2017-04-25 19:42:08 -0600

    many things

--- Macros/eqgame.bat
+++ Macros/eqgame.bat
@@ -1,5 +1,6 @@
 start /d "C:\Users\Jimbob\Downloads\IP" macroquest2.exe
 start /d "C:\Users\Jimbob\Downloads\IP" eqbcserver.exe
+
 start /d "C:\everquest UF" eqgame.exe patchme /login:bdens01
 start /d "C:\everquest UF" eqgame.exe patchme /login:bdens02
 start /d "C:\everquest UF" eqgame.exe patchme /login:bdens03
